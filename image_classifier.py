3e1ef1fc55420656cf0f92844a63951e10465dd0
aturcino <adnan.turcinovic@gmail.com>
2017-10-05 20:23:52 +0200

    add fwd prop

--- image_classifier.py
+++ image_classifier.py
@@ -38,3 +38,30 @@ def cost(logits, labels):
     sess.close()
     
     return cost
+	
+def forward_propagation(X, parameters):
+    """
+    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
+    
+    Arguments:
+    X -- input dataset placeholder
+    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"; tbd initialize params
+
+    Returns:
+    Z3 -- the output of the last LINEAR unit
+    """
+    
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+  
+    Z1 = tf.add(tf.matmul(W1,X), b1)                                              
+    A1 = tf.nn.relu(Z1)                                              
+    Z2 = tf.add(tf.matmul(W2,A1), b2)                                              
+    A2 = tf.nn.relu(Z2)                                              
+    Z3 = tf.add(tf.matmul(W3,A2), b3)                                
+    
+    return Z3
